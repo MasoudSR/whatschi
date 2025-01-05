a862e5771439de19cfc09c19a5d8b61bda4cedb2
Masoud S.R <amir.sepehr72@gmail.com>
2025-01-05 16:30:25 +0330

    font changed

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -5,7 +5,7 @@ function MenuBar({ page, setPage }) {
     return (
         <div className="flex w-full text-white select-none max-w-screen-md m-auto md:p-3 fixed top-0 left-[50%] -translate-x-[50%] z-20">
             <div className="grid grid-cols-3 bg-green-700 w-full text-center justify-items-center drop-shadow-sm items-center p-3 shadow-md md:rounded-full md:px-5">
-                <div className='justify-self-start'>Whatschi</div>
+                <div className='justify-self-start'>WhatsChi</div>
                 <div className='flex bg-green-800 rounded-3xl shadow-inner text-xs px-[3px]'>
                     <div className={`flex items-center justify-center transition-all w-[70px] h-[32px] z-20 ${page === "home" ? "text-green-800" : "cursor-pointer"}`} onClick={() => { setPage("home") }}>Home</div>
                     <div className={`flex items-center justify-center transition-all w-[70px] h-[32px] z-20 ${page === "contacts" ? "text-green-800" : "cursor-pointer"}`} onClick={() => { setPage("contacts") }}>Contacts</div>
